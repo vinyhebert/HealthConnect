741f7964e575511d9600a165c29751be58bf841f
DiegoDamiao <damiao.diego@gmail.com>
2023-09-20 23:48:04 -0300

    Clear

--- Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-Console.WriteLine("Hello, World!!!111");
-=======
-Console.WriteLine("Hello, World!!!!!");
->>>>>>> f19e017985635cccf314fb1cbf57a76627376627
