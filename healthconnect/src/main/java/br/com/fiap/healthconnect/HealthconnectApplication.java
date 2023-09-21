3352a1a7c51d08b4ed7a0087d8fdf851007021ef
DiegoDamiao <damiao.diego@gmail.com>
2023-09-20 23:38:25 -0300

    V2

---
+++ healthconnect/src/main/java/br/com/fiap/healthconnect/HealthconnectApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.healthconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthconnectApplication.class, args);
+	}
+
+}
---
+++ healthconnect/src/test/java/br/com/fiap/healthconnect/HealthconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.healthconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
